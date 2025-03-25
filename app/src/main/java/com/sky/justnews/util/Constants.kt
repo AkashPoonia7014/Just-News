7c05e7f9d506a6305471b867098a524ab2cfa100
Akash Poonia <akashpoonia70144@gmail.com>
2025-03-25 21:30:50 +0530

    removed api key

--- app/src/main/java/com/sky/justnews/util/Constants.kt
+++ app/src/main/java/com/sky/justnews/util/Constants.kt
@@ -3,7 +3,7 @@ package com.sky.justnews.util
 class Constants {
     companion object {
 
-        const val API_KEY = "a478b677ec674903a2d29ac83d117779"
+        const val API_KEY = "<Insert Your Api key here>"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
